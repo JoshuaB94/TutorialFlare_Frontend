922a506f276eee10de880b8c079fe7bc94cc70f1
Joshua Berrios <ixoyedesignstudio@gmail.com>
2021-12-27 23:09:40 +0000

    Edited typography for card component + Added React-Icons + Established Classes for new elements

--- src/components/VideoCreatorCard/VideoCreatorCard.jsx
+++ src/components/VideoCreatorCard/VideoCreatorCard.jsx
@@ -4,6 +4,7 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import { Avatar, Button, CardActionArea, CardActions } from '@mui/material';
+import { AiFillYoutube, AiOutlineTwitter, AiFillMail } from 'react-icons/ai'
 
 const VideoCreatorCard = () => {
   return (
@@ -11,17 +12,21 @@ const VideoCreatorCard = () => {
       <CardActionArea>
         <Avatar src="/broken-image.jpg" sx={{width: 150, height: 150}}/>
         <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
+          <Typography gutterBottom variant="h6" component="div">
+            John Doe
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
+            City, State
           </Typography>
+          <div>
+            <AiFillYoutube className="youtube-icon" />
+            <AiOutlineTwitter className="twitter-icon"/>
+            <AiFillMail className="mail-icon" />
+          </div>
         </CardContent>
       </CardActionArea>
       <CardActions>
-      <Button variant="contained" disableElevation size="small">View Profile</Button>
+      <Button variant="contained" disableElevation size="small" className="profile-button">View Profile</Button>
       </CardActions>
     </Card>
   );
