d075579438afd853c3465399170617a8cb71f60a
Joshua Berrios <64435634+JoshuaB94@users.noreply.github.com>
2021-12-30 21:39:48 -0600

    Coded CSS for RegisterCompany component and added classes to div and form tags

--- src/components/RegisterCompany/RegisterCompany.jsx
+++ src/components/RegisterCompany/RegisterCompany.jsx
@@ -21,7 +21,7 @@ const RegisterCompany = () => {
         let response = await axios.post(`http://localhost:3200/api/company/register`, postCompany);
         if (response.status === 200) {
             console.log(response.data);
-            window.location = '/login';
+            window.location = '/company-login';
         }
 
         if (response.status === 400) {
@@ -32,11 +32,14 @@ const RegisterCompany = () => {
     return ( 
         <div className="company-registration-page">
             <form className="company-registration-form" onSubmit={handleSubmit}>
-                <h1>Register as a Company</h1>
+                <h1 className="company-registration-heading">Register as a Company</h1>
                 <input placeholder="Your Company Name" type="text" value={company} onChange={(e) => registerCompany(e.target.value)}/>
                 <input placeholder="Email Address" type="text" value={email} onChange={(e) => registerEmail(e.target.value)}/>
                 <input placeholder="Enter a Password" type="text" value={password} onChange={(e) => registerPassword(e.target.value)}/>
                 <button className="company-register-button" type="submit">Register</button>
+                <div className="link-wrapper">
+                    <Link to="/company-login" className="goto-link">Already have an account? Click to login.</Link>
+                </div>
             </form>
         </div>
      );
