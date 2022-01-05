0d6dcfe18e89600a936e9bda2e3232d0e50a0607
Joshua Berrios <64435634+JoshuaB94@users.noreply.github.com>
2022-01-04 23:09:14 -0600

    Added the basic code for turning the ChatEngine into a direct messaging component.

--- src/components/ClientPortal/ClientPortal.jsx
+++ src/components/ClientPortal/ClientPortal.jsx
@@ -1,8 +1,28 @@
-import { ChatEngine } from 'react-chat-engine';
 import './ClientPortal.css';
+import { useState } from 'react';
 import NavBar from '../NavBar/NavBar';
+import { ChatEngine, getOrCreateChat } from 'react-chat-engine';
 
 const ClientPortal = () => {
+    const [username, setUsername] = useState("");
+
+    function createDirectChat(creds){
+        getOrCreateChat(
+            creds,
+            {is_direct_chat: true, usernames: [username]},
+            () => setUsername("")
+        )
+    }
+
+    function renderChatForm(creds){
+        return (
+            <div>
+                <input placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)}/>
+                <button onClick={() => createDirectChat(creds)}> Create New Chat</button>
+            </div>
+        )
+    }
+
     return ( 
         <NavBar />,
         <ChatEngine 
@@ -10,6 +30,7 @@ const ClientPortal = () => {
             projectID="e165ae9d-3188-4d54-8f9b-df7008e891c3"
             userName="JoshuaBerrios"
             userSecret="Nikon-1994"
+            renderNewChatForm={(creds) => renderChatForm(creds)}
         />
      );
 }
